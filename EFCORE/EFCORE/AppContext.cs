d61010c46e68604da7d120ee80a2d6d0e5f8f8a2
GarikHayrapetyan <44756744+GarikHayrapetyan@users.noreply.github.com>
2019-12-19 21:37:24 +0100

    Add files via upload

---
+++ EFCORE/EFCORE/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCORE
+{
+    public class AppContext:DbContext
+    {   
+        public DbSet<User> Users { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFCOREdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EFCORE/EFCORE/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace EFCORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User { Id=1, Name = "Tom", Age = 22 };
+            User user1 = new User { Id=2, Name = "Sam", Age = 21 };
+
+            Company company = new Company { Name = "Samsung" };
+            Phone phone = new Phone { Name="A6 2018",Price=850,Manufacturer=company};
+
+            using (AppContext db = new AppContext())
+            {
+                db.Phones.Add(phone);
+                db.SaveChanges();
+            }
+
+                Console.ReadKey();
+        }
+
+
+        static void Add(params User[] users)
+        {
+            using (AppContext db = new AppContext()) {
+                db.Add(users);
+                db.SaveChanges();
+            }
+        }
+
+        static void Delete(params User[] users)
+        {
+            using (AppContext db = new AppContext())
+            {
+                foreach(User user in users)
+                {
+                   db.Users.Remove(user);
+                }
+                
+                db.SaveChanges();
+            }
+        }
+
+        static void Read(){
+
+            using (AppContext db = new AppContext())
+            {
+                var users = db.Users.ToList();
+                foreach(User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+        }
+    }
+}
